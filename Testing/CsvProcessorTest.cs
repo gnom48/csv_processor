c8e8e5986834cd72a0b4209c9db0a884bf24acd8
gnom48 <gnom.48.spb@gmail.com>
2026-01-18 10:53:23 +0300

    Тесты

--- Testing/CsvProcessorTest.cs
+++ Testing/CsvProcessorTest.cs
@@ -2,7 +2,7 @@
 using CsvProcessor.Services;
 
 [TestClass]
-public sealed class CsvProcessorTest
+public sealed class CsvValidationTest
 {
     [TestMethod]
     public void MapAndThrowIfInvalid_ValidInput_ReturnsCorrectValueObject()
@@ -67,57 +67,5 @@ public sealed class CsvProcessorTest
 
         // Act & Assert
         Assert.ThrowsException<ValidationException>(() => Validations.MapAndThrowIfInvalid(negativeValueRow));
-    }
-
-    [TestMethod]
-    public void CalculateMedian_OddNumberOfElements_ReturnsMiddleElement()
-    {
-        // Arrange
-        var oddNumbers = new double[] { 1, 3, 5 };
-
-        // Act
-        var result = Validations.CalculateMedian(oddNumbers);
-
-        // Assert
-        Assert.AreEqual(3, result);
-    }
-
-    [TestMethod]
-    public void CalculateMedian_EvenNumberOfElements_ReturnsAverageOfTwoMiddleElements()
-    {
-        // Arrange
-        var evenNumbers = new double[] { 1, 2, 3, 4 };
-
-        // Act
-        var result = Validations.CalculateMedian(evenNumbers);
-
-        // Assert
-        Assert.AreEqual(2.5, result);
-    }
-
-    [TestMethod]
-    public void CalculateMedian_EmptyArray_ReturnsZero()
-    {
-        // Arrange
-        var emptyArray = Array.Empty<double>();
-
-        // Act
-        var result = Validations.CalculateMedian(emptyArray);
-
-        // Assert
-        Assert.AreEqual(0, result);
-    }
-
-    [TestMethod]
-    public void CalculateMedian_SingleElement_ReturnsThatElement()
-    {
-        // Arrange
-        var singleElementArray = new double[] { 10 };
-
-        // Act
-        var result = Validations.CalculateMedian(singleElementArray);
-
-        // Assert
-        Assert.AreEqual(10, result);
     }
 }
